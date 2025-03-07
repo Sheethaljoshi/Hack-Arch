1332623ea7cd5b488f535475998da2b078537354
Sheethal <sh33thal24@gmail.com>
2025-03-08 05:21:42 +0530

    .

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
-import axios from "axios";  // Import Axios
+import { useState } from "react"; // Import Axios
 import { motion } from "framer-motion";
 import { FaUser, FaLock } from "react-icons/fa";
 import axios from "axios";
